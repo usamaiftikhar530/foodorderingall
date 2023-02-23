3fc81ff14cb79cae2422a5a6452bd02fad2a98e2
usamaiftikhar530 <usamaiftikhar530@gmail.com>
2023-02-24 00:30:06 +0500

    third commit

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(require("./router/auth"));
 const PORT = process.env.PORT || 5000;
 
 //Static Files
-app.use(express.static(path.join(__dirname,"./server/client/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./server/client/build/index.html'))
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
 
